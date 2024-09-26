22749c53879a1b6a9ce249f96aab4b24f45ac7f0
valeriiamarshalova <leramarshalova@gmail.com>
2024-09-27 01:20:27 +0200

    first commit

---
+++ src/app/TotalSales.java
@@ -0,0 +1,30 @@
+package app;
+
+public class TotalSales {
+
+    static double numPrice;
+    static int numQuantity;
+    static double res1;
+    static double res2;
+
+
+    public static void main(String[] args){
+
+        String finishSymbol = ".";
+        String product = "Product No ";
+        int productNum = 1;
+        String productName = ": smartphone,";
+        String totalSales = "total sales for ";
+        String daysIs = " days is EUR ";
+        int salesNum = 5;
+        String sales = "sales by day is EUR ";
+
+        numPrice = 690.99;
+        numQuantity = 17;
+        res1 = numPrice * numQuantity;
+
+        System.out.println(product + productNum + productName + "\n" +
+                totalSales + salesNum + daysIs + res1 + finishSymbol + "\n" +
+                sales);
+    }
+}
